5816bdb22fe9baaff41c9fc47039ecfc70cf5b29
caique2210 <caiquesalmaso@gmail.com>
2024-10-02 22:55:09 +0000

    atualizando

--- hello.py
+++ hello.py
@@ -1,6 +1,15 @@
 # A very simple Flask Hello World app for you to get started with...
 from flask import Flask
 app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
-    return '<p>Hello from Flask!</p><table><tr><td><b>Professor:</b></td><td>Professor Fabio Teixeira</td></tr><tr><td><b>Prontuário:</b></td><td>PT23820X</td></tr></table>'
+    return '<h1> Hello, World!</h1>'
+
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1> Hello, {}</h1>'.format(name)
+
+@app.route('/contextorequisicao')
+def user(name):
