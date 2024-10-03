6dd0c440cb31bb3f7f23ea109a82acc845bbb47c
caique2210 <caiquesalmaso@gmail.com>
2024-10-03 00:10:32 +0000

    att1

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 # A very simple Flask Hello World app for you to get started with...
-from flask import Flask
+from flask import Flask, request, redirect, abort, jsonify
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,4 +12,23 @@ def user(name):
     return '<h1> Hello, {}</h1>'.format(name)
 
 @app.route('/contextorequisicao')
-def user(name):
+def contexto_requisicao():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is {}</p>'.format(user_agent)
+
+@app.route('/codigostatusdiferente')
+def codigo_status_diferente():
+    return "Bad Request", 400
+
+@app.route('/objetoresposta')
+def objeto_resposta():
+    return "<h1>This document carries a cookie!</h1>", 200, {'Set-Cookie': 'cookie_name=cookie_value'}
+
+@app.route('/redirecionamento')
+def redirecionamento():
+    return redirect("https://ptb.ifsp.edu.br/")
+
+@app.route('/abortar')
+def abortar():
+    abort(404)
+
