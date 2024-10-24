68a77fc665e9bbbf67cb9e995911e572c583cea1
caique2210 <caiquesalmaso@gmail.com>
2024-10-24 01:55:18 +0000

    Semana 4

--- hello.py
+++ hello.py
@@ -1,29 +1,99 @@
-from flask import Flask, render_template
-from datetime import datetime, timedelta
+from flask import Flask, request, redirect, url_for
+from datetime import datetime
 
 app = Flask(__name__)
 
-# Função para ajustar o horário de Brasília
-def get_brasilia_time():
-    now_utc = datetime.utcnow()  # Hora UTC
-    brasilia_time = now_utc - timedelta(hours=3)  # Ajuste de 3 horas para o horário de Brasília
-    return brasilia_time.strftime("%B %d, %Y %I:%M %p")
-
-# Rota principal
 @app.route('/')
 def home():
-    formatted_time = get_brasilia_time()
-    return render_template('index.html', time=formatted_time)
+    current_time = datetime.now().strftime("%B %d, %Y %I:%M %p")
+    return '''
+    <html>
+        <head>
+            <link rel="stylesheet" type="text/css" href="/static/style.css">
+            <title>Avaliação contínua</title>
+        </head>
+        <body>
+            <div class="navbar">
+                <ul>
+                    <li><a href="/">Avaliação contínua: Aula 040</a></li>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/identificacao">Identificação</a></li>
+                    <li><a href="/contextorequisicao/Caique%20Salmaso">Contexto de requisição</a></li>
+                </ul>
+            </div>
+            <div class="content">
+                <h2>Dados da última utilização:</h2>
+                <hr>
+                <p>The local date and time is {}<br>
+                That was in a few seconds.</p>
+            </div>
+        </body>
+    </html>
+    '''.format(current_time)
 
-# Rota de usuário sem Hello World
-@app.route('/user/<name>')
-def user(name):
-    return render_template('index.html', user=name)
+@app.route('/identificacao')
+def identificacao():
+    return redirect(url_for('user', name="Caique Salmaso", prontuario="PT3026663", instituicao="IFSP PIRITUBA"))
 
-# Rota não encontrada sem Hello World
-@app.route('/rotainexistente')
-def not_found():
-    return render_template('index.html', error=True)
+@app.route('/user/<name>/<prontuario>/<instituicao>')
+def user(name, prontuario, instituicao):
+    return '''
+    <html>
+        <head>
+            <link rel="stylesheet" type="text/css" href="/static/style.css">
+            <title>Identificação</title>
+        </head>
+        <body>
+            <div class="navbar">
+                <ul>
+                    <li><a href="/">Avaliação contínua: Aula 040</a></li>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/identificacao">Identificação</a></li>
+                    <li><a href="/contextorequisicao/Caique%20Salmaso">Contexto de requisição</a></li>
+                </ul>
+            </div>
+            <div class="content">
+                <h2 style="font-weight: normal;">Olá, {}!</h2>
+                <hr>
+                <h2 style="font-weight: normal;">Prontuário: {}</h2>
+                <h2 style="font-weight: normal;">Instituição: {}</h2>
+            </div>
+        </body>
+    </html>
+    '''.format(name, prontuario, instituicao)
+
+@app.route('/contextorequisicao/<name>')
+def context(name):
+    user_agent = request.headers.get('User-Agent')
+    remote_ip = request.remote_addr
+    host = request.host
+
+    return '''
+    <html>
+        <head>
+            <link rel="stylesheet" type="text/css" href="/static/style.css">
+            <title>Contexto de Requisição</title>
+        </head>
+        <body>
+            <div class="navbar">
+                <ul>
+                    <li><a href="/">Avaliação contínua: Aula 040</a></li>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/identificacao">Identificação</a></li>
+                    <li><a href="/contextorequisicao/Caique%20Salmaso">Contexto de requisição</a></li>
+                </ul>
+            </div>
+            <div class="content">
+                <h2 style="font-weight: normal;">Olá, {}!</h2>
+                <hr>
+                <p>Seu navegador é: {}</p>
+                <p>O IP do computador remoto é: {}</p>
+                <p>O host da aplicação é: {}</p>
+            </div>
+        </body>
+    </html>
+    '''.format(name, user_agent, remote_ip, host)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+
