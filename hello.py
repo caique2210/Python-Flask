89ef7de7c7fc886d1f951a6046ecbf10372884b2
caique2210 <caiquesalmaso@gmail.com>
2024-10-31 01:55:22 +0000

    Semana 05

--- hello.py
+++ hello.py
@@ -1,98 +1,63 @@
-from flask import Flask, request, redirect, url_for
-from datetime import datetime
+from flask import Flask, request, render_template_string
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    current_time = datetime.now().strftime("%B %d, %Y %I:%M %p")
-    return '''
+    return render_template_string('''
     <html>
         <head>
             <link rel="stylesheet" type="text/css" href="/static/style.css">
-            <title>Avaliação contínua</title>
+            <title>Flasky</title>
         </head>
         <body>
             <div class="navbar">
                 <ul>
-                    <li><a href="/">Avaliação contínua: Aula 040</a></li>
+                    <li><a href="/">Flasky</a></li>
                     <li><a href="/">Home</a></li>
-                    <li><a href="/identificacao">Identificação</a></li>
-                    <li><a href="/contextorequisicao/Caique%20Salmaso">Contexto de requisição</a></li>
                 </ul>
             </div>
             <div class="content">
-                <h2>Dados da última utilização:</h2>
+                <h2>Hello, {{ name }}!</h2>
                 <hr>
-                <p>The local date and time is {}<br>
-                That was in a few seconds.</p>
+                <form method="post" action="/setname">
+                    <label for="name"><b>What is your name?</b></label><br>
+                    <input type="text" id="name" name="name" required style="width: 90%; height: 30px;"><br><br>
+                    <button type="submit">Submit</button>
+                </form>
             </div>
         </body>
     </html>
-    '''.format(current_time)
+    ''', name="Caique")  # Nome padrão como "Caique"
 
-@app.route('/identificacao')
-def identificacao():
-    return redirect(url_for('user', name="Caique Salmaso", prontuario="PT3026663", instituicao="IFSP PIRITUBA"))
-
-@app.route('/user/<name>/<prontuario>/<instituicao>')
-def user(name, prontuario, instituicao):
-    return '''
+@app.route('/setname', methods=['POST'])
+def set_name():
+    name = request.form['name']
+    return render_template_string('''
     <html>
         <head>
             <link rel="stylesheet" type="text/css" href="/static/style.css">
-            <title>Identificação</title>
+            <title>Flasky</title>
         </head>
         <body>
             <div class="navbar">
                 <ul>
-                    <li><a href="/">Avaliação contínua: Aula 040</a></li>
+                    <li><a href="/">Flasky</a></li>
                     <li><a href="/">Home</a></li>
-                    <li><a href="/identificacao">Identificação</a></li>
-                    <li><a href="/contextorequisicao/Caique%20Salmaso">Contexto de requisição</a></li>
                 </ul>
             </div>
             <div class="content">
-                <h2 style="font-weight: normal;">Olá, {}!</h2>
+                <h2>Hello, {{ name }}!</h2>
                 <hr>
-                <h2 style="font-weight: normal;">Prontuário: {}</h2>
-                <h2 style="font-weight: normal;">Instituição: {}</h2>
+                <form method="post" action="/setname">
+                    <label for="name"><b>What is your name?</b></label><br>
+                    <input type="text" id="name" name="name" required style="width: 90%; height: 30px;"><br><br>
+                    <button type="submit">Submit</button>
+                </form>
             </div>
         </body>
     </html>
-    '''.format(name, prontuario, instituicao)
-
-@app.route('/contextorequisicao/<name>')
-def context(name):
-    user_agent = request.headers.get('User-Agent')
-    remote_ip = request.remote_addr
-    host = request.host
-
-    return '''
-    <html>
-        <head>
-            <link rel="stylesheet" type="text/css" href="/static/style.css">
-            <title>Contexto de Requisição</title>
-        </head>
-        <body>
-            <div class="navbar">
-                <ul>
-                    <li><a href="/">Avaliação contínua: Aula 040</a></li>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/identificacao">Identificação</a></li>
-                    <li><a href="/contextorequisicao/Caique%20Salmaso">Contexto de requisição</a></li>
-                </ul>
-            </div>
-            <div class="content">
-                <h2 style="font-weight: normal;">Olá, {}!</h2>
-                <hr>
-                <p>Seu navegador é: {}</p>
-                <p>O IP do computador remoto é: {}</p>
-                <p>O host da aplicação é: {}</p>
-            </div>
-        </body>
-    </html>
-    '''.format(name, user_agent, remote_ip, host)
+    ''', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
